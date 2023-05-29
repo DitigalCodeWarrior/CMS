66f27d72e91822e2663e842726dbc1d79d321932
CodeRanger2023 <codehunter2023@gmail.com>
2023-05-29 22:07:15 +0300

    database connection

---
+++ includes/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$connection = mysqli_connect('localhost', 'root', '', 'cms');
+
+if(!$connection) {
+    die("DATABASE CONNECTION FAILED" .mysqli_error($connection));
+} else {
+    echo "We are connected!";
+}
