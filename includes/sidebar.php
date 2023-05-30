3547ad37b1e274bb154859051a0a78f19b2b683b
CodeRanger2023 <codehunter2023@gmail.com>
2023-05-30 08:11:16 +0300

    create customized search

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,16 +1,33 @@
 <div class="col-md-4">
 
+
+        <?php
+
+        if(isset($_POST['submit'])) {
+            echo   $search =  $_POST['search'];
+
+        }
+
+
+
+
+        ?>
+
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+                    <form action="" method="post">
+
+                    
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input type="text" name="search" class="form-control">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button class="btn btn-default" type="submit" name="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+                    </form>
                     <!-- /.input-group -->
                 </div>
 
